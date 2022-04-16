5b0dc0f208e3337baecbf38c332a8fcb657bab66
PurpleSSSnake <56314743+PurpleSSSnake@users.noreply.github.com>
2022-04-16 16:05:34 +0300

    Charts

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -64,7 +64,9 @@ class ChartingState extends MusicBeatState
 		'Hey!',
 		'Hurt Note',
 		'GF Sing',
-		'No Animation'
+		'No Animation',
+                'Bullet',
+                'Candy'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
